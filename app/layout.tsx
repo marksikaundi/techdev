1c2367d65150faf8d89af493e49228286294ab26
marksikaundi <emmanuelmark117@gmail.com>
2025-07-14 00:55:37 +0000

    🔄 Update Footer import to use local components folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
-import { Footer } from "react-day-picker";
+import { Footer } from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
